3a36e4c63746ea39846eb09095c228fccb489a50
Marina Tislenko <26051978m@gmail.com>
2022-06-11 16:40:33 +0300

    My first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int cost (int price) {
+        int cost;
+        int calculate;
+        calculate = price/20;
+                return calculate;
+    }
+}
